3c866d6e8a0906311c63c3cf26aac639ff6ebc27
ReEnvisionAi <168676352+ReEnvisionAi@users.noreply.github.com>
2025-05-20 22:17:03 -0700

    Remove useFormStatus hook from Fieldset component

--- components/fieldset.tsx
+++ components/fieldset.tsx
@@ -1,14 +1,11 @@
 import { ComponentProps } from "react";
-import { useFormStatus } from "react-dom";
 
 export default function Fieldset({
   children,
   ...rest
 }: ComponentProps<"fieldset">) {
-  const { pending } = useFormStatus();
-
   return (
-    <fieldset {...rest} disabled={pending}>
+    <fieldset {...rest}>
       {children}
     </fieldset>
   );
