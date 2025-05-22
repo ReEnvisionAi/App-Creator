58212766cdbda96780d2ea3367baa3f72eaf78e8
ReEnvisionAi <168676352+ReEnvisionAi@users.noreply.github.com>
2025-05-22 13:27:25 -0700

    Fix animejs import and add dependency

--- components/notification/index.tsx
+++ components/notification/index.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useRef, useState } from 'react'
-import anime from 'animejs/lib/anime.es.js'
+import anime from 'animejs'
 import { X } from 'lucide-react'
 
 type NotificationType = 'success' | 'error' | 'info'
